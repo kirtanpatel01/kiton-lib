8c0a1fe8f464ecab75ee066d384f4bc374eb0d3a
Kirtan Patel <159251485+kirtanpatel01@users.noreply.github.com>
2024-12-05 08:25:10 +0530

    Enabled horizontal scroll CodeBlock.jsx

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -15,7 +15,7 @@ const CodeBlock = ({ language, code, single=true, className='' }) => {
   const savedTheme = localStorage.getItem("theme");
 
   return (
-    <div className={`flex ${className}`}>
+    <div className={`flex ${className} overflow-x-auto`}>
       <SyntaxHighlighter
         language={language}
         style={vscDarkPlus}
